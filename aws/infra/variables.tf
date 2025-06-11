b0749fa52a62c699ba95b04c1cb02b86007a4910
Sato, I <8ato2h1@gmail.com>
2025-06-11 12:26:52 +0000

    test

--- aws/infra/variables.tf
+++ aws/infra/variables.tf
@@ -12,6 +12,6 @@ variable "tags" {
 
 variable "aws_region" {
   type        = string
-  default     = "us-east-2"
+  default     = "ap-northeast-1"
   description = "AWS region for all resources"
 }
