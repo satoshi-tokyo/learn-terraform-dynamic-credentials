033a7860ed0349574a1e3b7655b93a57cf5e262e
Sato, I <8ato2h1@gmail.com>
2025-06-11 12:36:54 +0000

    test

--- aws/infra/main.tf
+++ aws/infra/main.tf
@@ -15,15 +15,7 @@ data "aws_ami" "amazon_linux" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
-
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo yum update -y
-    sudo yum install httpd -y
-    sudo systemctl enable httpd
-    sudo systemctl start httpd
-    echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
-    EOF
-
-  tags = var.tags
+  tags = {
+    Name = var.instance_name
+  }
 }
